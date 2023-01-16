d6a48bccf5e1809d9f634c6322b723f3f7f17b3a
Albert <albert.rocha@r2t.company>
2023-01-16 18:23:38 -0300

    feature: created user database schema

--- src/database/schemas/users.rs
+++ src/database/schemas/users.rs
@@ -3,11 +3,11 @@ use diesel::table;
 table! {
     users (id) {
         id -> Uuid,
-        first_name -> Text,
-        last_name -> Text,
-        email -> Text,
-        created_at -> Timestamp,
-        updated_at -> Timestamp,
-        deleted_at -> Timestamp,
+        first_name -> VarChar,
+        last_name -> VarChar,
+        email -> VarChar,
+        created_at -> Timestamptz,
+        updated_at -> Timestamptz,
+        deleted_at -> Option<Timestamptz>,
     }
 }

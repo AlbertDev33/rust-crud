729ad3cae3b2f22c7696b350a082472e848a5054
Albert <albert.rocha@r2t.company>
2023-01-16 18:26:37 -0300

    feature: main function

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,12 @@ use actix_web::{web, App, HttpServer};
 #[actix_rt::main]
 async fn main() -> Result<()> {
     set_var("RUST_LOG", "actix_web=debug");
+    
     HttpServer::new(move || {
         App::new()
-            .route("/users", web::get().to(handlers::get_users))
+            .service(web::resource("/users").route(web::get().to(handlers::get_users)))
+            .service(web::resource("/add-users").route(web::post().to(handlers::add_user)))
             .route("/users/{id}", web::get().to(handlers::get_user_by_id))
-            .route("/users", web::post().to(handlers::add_user))
             .route("/users/{id}", web::delete().to(handlers::delete_user))
     })
     .bind("127.0.0.1:3333")?
